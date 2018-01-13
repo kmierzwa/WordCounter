5838f4cd7c2fa5004526bd7564eb9c2b284949a7
kmierzwa <karolmierzwax@gmail.com>
2018-01-13 16:32:16 +0100

    removed and added words options

--- WordCounter.py
+++ WordCounter.py
@@ -1,39 +1,36 @@
 """25 most popular english words in txt"""
 import sys
 
-
 def Counter(subtitleFile, questedWordsFile):
-        subWords = open(subtitleFile).read().split(' ')
-        qwWords = open(questedWordsFile).read().replace('\n','').split(',')
-        wordsCounter = dict()
         qwDict = {}
 
         for qw in qwWords:
              qwDict.update({qw:subWords.count(qw.strip())})
         print qwDict
-        
-        
-       
+
+def ChangeFile(s, f, mode):
+        orig_stdout = sys.stdout
+        changeFile = open(f, mode)
+        sys.stdout = changeFile
+        print s
+        sys.stdout = orig_stdout
+        changeFile.close()
+                
 subFile = "F1_T4_Subtitle - English.srt"
-
-       
-
-
 qwFile = "out.txt"
 
-#print handle.read()
+subWords = open(subFile).read().split(' ')
+qwWords = open(qwFile).read().replace('\n','').replace(' ','').split(',')
 
-Counter(subFile, qwFile)
+user_input = raw_input('add new word to the list, if word exists would be removed ').replace('\n','')
 
-user_input = raw_input('Give me a word: ')
-orig_stdout = sys.stdout
-appendentWord = open('out.txt', 'a')
+if user_input in qwWords:
+       answear = raw_input("Ar u wsiur?(Y/N)")
+       if answear == 'Y':
+               qwWords.remove(user_input)
+               ChangeFile(",".join(qwWords), qwFile, 'w')
+else:
+        ChangeFile(','+user_input, qwFile, 'a')
 
-sys.stdout = appendentWord
-
-print ',',user_input
-sys.stdout = orig_stdout
-appendentWord.close()
-
-Counter(subFile, qwFile)
+Counter(subWords, qwWords)
 
